7eeb2137aa4fefceb3ca226893187a7d4bc5b940
paridhika <paridhika.kayal@gmail.com>
2020-02-16 00:21:31 -0800

    cleanup

--- installation_files/cleanup.sh
+++ installation_files/cleanup.sh
@@ -6,4 +6,18 @@ sudo rm -rf /var/lib/etcd
 systemctl daemon-reload
 systemctl enable kubelet && systemctl start kubelet
 
+
+
+
 netstat -natp | grep LISTEN
+
+systemctl daemon-reload
+systemctl stop kubelet
+systemctl stop docker
+systemctl stop iptables
+systemctl start iptables
+systemctl start docker
+systemctl start kubelet
+
+systemctl status kubelet
+
