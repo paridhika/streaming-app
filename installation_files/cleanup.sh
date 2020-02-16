99e0004ad45980091a4f9acefb8426e2998f23a2
paridhika kayal <paridhika.kayal@gmail.com>
2020-02-16 03:30:36 -0500

    yamls

--- installation_files/cleanup.sh
+++ installation_files/cleanup.sh
@@ -6,7 +6,11 @@ sudo rm -rf /var/lib/etcd
 systemctl daemon-reload
 systemctl enable kubelet && systemctl start kubelet
 
-
+systemctl --failed
+systemctl --failed --all
+systemctl reset-failed
+systemctl status kubelet.service
+# systemctl disable kubelet.service
 
 
 netstat -natp | grep LISTEN
