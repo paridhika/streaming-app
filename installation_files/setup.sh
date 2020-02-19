f074f55e59dba5017efac9b13668562700fc6f3d
paridhika <paridhika.kayal@gmail.com>
2020-02-19 06:09:37 -1000

    setup weave

--- installation_files/setup.sh
+++ installation_files/setup.sh
@@ -3,6 +3,19 @@
 
 sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
 sudo apt install selinux-utils
+sudo apt-get install ufw
+sudo ufw disable
+sudo curl -L git.io/weave -o /usr/local/bin/weave
+sudo chmod a+x /usr/local/bin/weave
+export CHECKPOINT_DISABLE=1
+mkdir -p /opt/cni/bin
+mkdir -p /etc/cni/net.d
+
+echo Adding " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" to /boot/cmdline.txt
+sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
+orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
+echo $orig | sudo tee /boot/cmdline.txt
+
 reboot
 setenforce 0
 
@@ -27,9 +40,4 @@ sudo update-rc.d dphys-swapfile remove
 
 sudo swapon --summary
 
-echo Adding " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" to /boot/cmdline.txt
-sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
-orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
-echo $orig | sudo tee /boot/cmdline.txt
-
-sudo reboot
+weave setup
