cbf47ed5e5a8848a7828a1c624291855eb3f6608
paridhika <paridhika.kayal@gmail.com>
2020-02-19 06:12:28 -1000

    setup weave

--- installation_files/setup.sh
+++ installation_files/setup.sh
@@ -41,3 +41,30 @@ sudo update-rc.d dphys-swapfile remove
 sudo swapon --summary
 
 weave setup
+
+## Create /etc/docker directory.
+
+mkdir /etc/docker
+
+# Configure the Docker daemon
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart Docker
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
