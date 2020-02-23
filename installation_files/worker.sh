0c675fb0d62dba9ed51391df2758776bb6c32ca8
paridhika kayal <paridhika.kayal@gmail.com>
2020-02-23 10:10:41 -0500

    ufw rules

--- installation_files/worker.sh
+++ installation_files/worker.sh
@@ -1,4 +1,9 @@
 sudo ufw allow 30000:40000/tcp
 sudo ufw allow 30000:40000/udp
+sudo ufw allow http
+sudo allow https
+sudo allow 3000
+sudo allow 8001:8010/tcp
+sudo allow 8001:8010/udp
 
 scp -r pi@142.150.235.54:/home/pi/.kube /home/pi
